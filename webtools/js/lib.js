0981f89adf926a21a9dac13731e621a2e6ea5252
Koen Beek <koen.beek@gmail.com>
2023-07-09 17:13:26 +0200

    move use of addArEl completely in lib

--- webtools/js/lib.js
+++ webtools/js/lib.js
@@ -14,30 +14,32 @@ function ranNum(len) { let n = ranTxt(len, '0123456789'); return len < 16 ? pars
 function createEl(typ, attrs) { let el = document.createElement(typ); Object.assign(el, attrs); return el } // create a html document element
 function addChild(typ, attrs, id) { gE(id).appendChild(createEl(typ, attrs)) } // add a child element to an existing html document element
 function addArEl(pid, i, txt, siz) { let id = pid + i; addChild("div", { id: "div" + id, innerHTML: '<label for="' + id + '">' + txt + i + ':&nbsp;</label><input type="text" id="' + id + '" size=' + siz + ' oninput="run()"><br>' }, pid + "s") } // add html element to array
-function mngAr(id, nbr, txt, siz) { // manage nbr of elements in input array id
-  while ((nbr > 1) && ((gV(id + nbr) == "") && (gV(id + (nbr - 1)) == ""))) { gE(id + "s").removeChild(gE("div" + id + nbr--)) }
-  if (gV(id + nbr) != '') { addArEl(id, ++nbr, txt, siz) }
-  return nbr
-}
 function intxt(i) { return createEl("div", { innerHTML: "<h2>Input <button onclick='fromCP()'>Get From Clipboard</button><button onclick='gE(\"" + "infile" + "\").click()'>Upload File</button><input type='file' style='display:none;' id='infile' onchange='doUL()'/></h2><textarea id='intext' rows='" + i[0] + "' cols='" + i[1] + "'></textarea>" }) }
 function outxt(o) { return createEl("div", { innerHTML: "<h2>Output <button onclick='toCP()'>Copy To Clipboard</button><button onclick='doDL()'>Download</button></h2><textarea id='outtext' rows='" + o[0] + "' cols='" + o[1] + "' readonly></textarea>" }) }
-function doUL() { let r = new FileReader(); r.onload = e => { gE('intext').value = e.target.result; f() }; r.readAsText(gE("infile").files[0]) }
+function doUL() { let r = new FileReader(); r.onload = e => { gE('intext').value = e.target.result; f() }; r.readAsText(gE("infile").files[0]) } // upload file as intext
 function doDL() { createEl("a", { href: "data:x-application/text," + escape(gV("outtext")), download: 'output.txt' }).click() } // download outtext as file
+function mngAr(ars) { // manage nbr of elements in input arrays
+  let ar = ars
+  return () => { // return closure
+    ar.forEach(a => { // for each array : remove last array elements if empty, leave 1 empty array element at end
+      while ((a.n > 1) && ((gV(a.i + a.n) == "") && (gV(a.i + (a.n - 1)) == ""))) { gE(a.i + "s").removeChild(gE("div" + a.i + a.n--)) }
+      if (gV(a.i + a.n) != '') { addArEl(a.i, ++a.n, a.t, a.s) }
+    })
+  }
+}
 async function doSetup(f, out, inp, ar = []) { // set up the webpage
-  let ret = []
   Object.assign(window, { run: f, toCP: toCP, fromCP: fromCP, doDL: doDL, doUL: doUL, gE: gE }) // make sure document sees the functions
   if (inp) { document.body.appendChild(intxt(inp)) } // create the intext text area and related heading and buttons
   if (out) { document.body.appendChild(outxt(out)) } // create the outtext text area and related heading and buttons
   const p = new URLSearchParams(document.location.search) // capture URL parameters and apply them to the relevant elements
-  if (p.has("paramfile")) { const js = await (await fetch(p.get("paramfile"))).json(); Object.keys(js).forEach(k => p.append(k, js[k])) } // get parameters from a parameter file in JSON format    
-  ar.forEach(a => { let i = 1; do { addArEl(a.id, i, a.t, a.s) } while (p.has(a.id + (i++))); ret.push(--i) }); // handle arrayed input texts - a is like { id: "xp", t: "XPath ", s: "40" }
+  if (p.has("paramfile")) { try { const js = await (await fetch(p.get("paramfile"))).json(); Object.keys(js).forEach(k => p.append(k, js[k])) } catch (e) { } } // get parameters from a parameter file in JSON format 
+  ar.forEach(a => { let i = 1; do { addArEl(a.i, i, a.t, a.s) } while (p.has(a.i + (i++))); a.n = --i; }); // handle arrayed input texts - a is like { i: "xp", t: "XPath ", s: "40" }
+  let mngArs = mngAr(ar); window.run = () => { mngArs(); f(); }
   ["input[type = 'text']", "#intext", "input[type='checkbox']", "select"].forEach(q => { // set input field value to parameter value
     document.querySelectorAll(q).forEach(e => {
       if (p.has(e.id)) { e.type === 'checkbox' ? e.checked = (p.get(e.id) == "on") : e.value = p.get(e.id) }
       gE(e.id).addEventListener("input", f) // call run() when content changes
     })
   })
-  return ret
 }
-export { gE, gV, gC, doSetup, sErr, sOK, ran, ranTxt, ranNum, mngAr }
-// Todo: move use of addArEl completely in lib and avoid caller to need to handle/call this by including it in run() automatically in dosetup
+export { gE, gV, gC, doSetup, sErr, sOK, ran, ranTxt, ranNum }
